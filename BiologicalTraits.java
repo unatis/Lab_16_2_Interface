7e4b5191cedc4b6100af1b0e86c618d1cf323a15
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 16:27:41 +0200

    Create BiologicalTraits.java

---
+++ BiologicalTraits.java
@@ -0,0 +1,12 @@
+package com.company;
+
+interface BiologicalTraits {
+
+    public void Eat(); // interface method
+
+    public void Digestion(); // interface method
+
+    public void Walk();
+
+
+}
