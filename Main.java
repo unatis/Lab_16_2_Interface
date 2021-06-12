35d3a79be0356d1e90c929acd2bfcb7ff62afb25
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 16:27:07 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pig pig = new Pig();
+
+        pig.SomeDefaultMethod();
+        pig.animalSound();
+        pig.sleep();
+
+        pig.Eat();
+        pig.Digestion();
+        pig.Walk();
+
+        pig.PigMethod();
+
+    }
+
+}
